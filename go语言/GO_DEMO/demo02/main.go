346f9208f086812bcfab5d3131aee30384c45580
xuhailong <xuhailong@edianyun.com>
2025-09-11 23:08:16 +0800

    go

---
+++ go语言/GO_DEMO/demo02/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("A")
+	// fmt.Println("B")
+	// fmt.Println("C") //换行，一次输出多句话时有空格
+	// fmt.Print("D") //一次输出多句话，无空格
+	// fmt.Printf("E")
+	// var a int = 10
+	// var b int = 3
+	// var c int = 5
+
+	// fmt.Println("a =", a, "b =", b, "c =", c)
+	//fmt.Printf("a=%v b=%v c=%v", a, b, c)
+	// a := 10
+	// print(a)
+
+	var name string
+	fmt.Print(name)
+
+}
